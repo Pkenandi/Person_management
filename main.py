a970ee2002a5255879cbde3e26c7c6bbed0004d4
Prince <kenandiprince@yahoo.com>
2022-02-25 10:29:56 +0100

    Complet operations for woman class and fix bugs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from men import Man
+from women.Woman import Woman
 
 choice = 0
 Men = []
@@ -14,11 +15,11 @@ while choice != 3:
 
     if choice == 1:
         picked = 0
-        instance = Man.Man()
+        instance = Man.Man(None, None, None, None)
         while picked != 6:
             print("   # ******************************************** #")
             print("   # 1. Add a man ")
-            print("   # 2. Edit a man ")
+            print("   # 2. Edit by matricule ")
             print("   # 3. Delete by matricule ")
             print("   # 4. Display by matricule ")
             print("   # 5. Display all ")
@@ -57,16 +58,43 @@ while choice != 3:
 # ------------------------------------------------------------------------------------------------------------------
     elif choice == 2:
         picked = 0
+        instance = Woman(None, None, None, None)
         while picked != 6:
             print("   # ******************************************** #")
-            print("   # 1. Add a woman ")
-            print("   # 2. Edit a woman ")
-            print("   # 3. Delete a woman ")
-            print("   # 4. Display by first name ")
+            print("   # 1. Add a women ")
+            print("   # 2. Edit by cin ")
+            print("   # 3. Delete by cin ")
+            print("   # 4. Display by cin ")
             print("   # 5. Display all ")
             print("   # 6. Back <- ")
             print("   # ___________________________________________ #")
             picked = int(input("  ==> : "))
             # -----------------------------------------------------------------------------------------
             if picked == 1:
-                print(" Add a man was picked ")
+                print(" ------ Add Operation ------- ")
+                result = instance.saisir()
+                instance.save(Women, result)
+                print(" ------ End Operation ------- ")
+
+            if picked == 2:
+                print(" ------ Edit Operation ------- ")
+                cin = input(" Entrez le cin de la personne : ")
+                instance.edit(Women, cin)
+                print(" ------ End Operation ------- ")
+
+            if picked == 3:
+                print(" ------ Delete Operation ------- ")
+                cin = input(" Entrez le cin de la personne : ")
+                instance.delete(Women, cin)
+                print(" ------ End Operation ------- ")
+
+            if picked == 4:
+                print(" ------ Display by Cin ------- ")
+                cin = input(" Entrez le cin de la personne : ")
+                instance.display_by(Women, cin)
+                print(" ------ End Operation ------- ")
+
+            if picked == 5:
+                print(" ------ Display All ------- ")
+                instance.display_all(Women)
+                print(" ------ End Operation ------- ")
